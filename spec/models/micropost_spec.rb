5bdbd857d71c2a3f07a7e4221a68db37045e4e8a
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-09-27 15:15:38 -0400

    Add micropost spec.

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,4 +1,9 @@
 require 'spec_helper'
 
 describe Micropost do
+  it { should belong_to(:user) } 
+  it { should validate_presence_of(:content) }
+  it { should validate_presence_of(:user_id) }
+  it { should_not allow_value("x" * 200).for(:content) }
+  it { should allow_value("x" * 139).for(:content) }
 end
