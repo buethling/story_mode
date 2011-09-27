03acb7428878424a51ae031eaf7a8b192b521c91
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-09-27 14:38:04 -0400

    Add shoulda_matchers, kill default pending tests and add one for character.

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Character do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) } 
+  it { should belong_to(:story) } 
+  it { should validate_presence_of(:story_id) } 
+  it { should validate_presence_of(:user_id) } 
 end
