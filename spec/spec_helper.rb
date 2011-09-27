648b14c02264a5a4dcdc5b78ee4bebcf124b459c
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-09-27 15:32:28 -0400

    Add simplecov.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,9 @@ require 'rspec/rails'
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
+require 'simplecov'
+SimpleCov.start
+
 RSpec.configure do |config|
   # == Mock Framework
   #
