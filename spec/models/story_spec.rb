d5884efc1ab317685103011a4cb3d258728f635f
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-09-28 12:18:15 -0400

    Story spec.

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -1,4 +1,12 @@
 require 'spec_helper'
 
 describe Story do
+  it { should belong_to(:user) }
+  it { should have_many(:phrases) }
+  it { should have_many(:characters) }
+  it { should have_many(:followers) }
+  it { should validate_presence_of(:user_id) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:blurb) }
+  it { should validate_presence_of(:character_count) }
 end
