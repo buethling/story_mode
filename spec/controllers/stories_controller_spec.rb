88d4914dfeb5e62c67c91c153caa6189e3332c88
Greg Maccarone <gmaccarone@gmail.com>
2011-09-29 12:36:21 -0700

    Story controller test fix

--- spec/controllers/stories_controller_spec.rb
+++ spec/controllers/stories_controller_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
 describe StoriesController do
-  it "should redirect to signin_path if not logged in" do
-    get "/stories/1"
-    response.should redirect_to signin_path
+  context "for a non-logged in user" do
+    before do
+      get :show, :id => 1
+    end
+
+    it { should redirect_to(signin_path) }
   end
 end
