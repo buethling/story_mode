85174d7f07865cf13428d182d06a0d14069ab563
Chris Lydon <chris.lydon@gmail.com>
2011-09-29 15:59:06 -0300

    Updating the Stories spec.

--- spec/controllers/stories_controller_spec.rb
+++ spec/controllers/stories_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe StoriesController do
   it "should redirect to signin_path if not logged in" do
-    get :show
-    assert_redirected_to signin_path
+    get "/stories/1"
+    response.should redirect_to signin_path
   end
 end
